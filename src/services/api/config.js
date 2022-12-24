276ba62f55653eb597718c4bac3bb35683ddc962
hacthe-codelink <the.duong@codelink.io>
2022-12-24 17:16:44 +0700

    update url backend deployed

--- src/services/api/config.js
+++ src/services/api/config.js
@@ -1,6 +1,7 @@
 
 // API URL
-const URL_MAIN_BE = "http://localhost:5005/";
+// const URL_MAIN_BE = "http://localhost:5005/";
+const URL_MAIN_BE = "https://medigood-be.onrender.com";
 const URL_SYSTEM_V1 = URL_MAIN_BE + "api";
 // const URL_AI_CHAT_BOT = "http://localhost:5001";
 const URL_AI_CHAT_BOT = "https://ai-chat-bot-predict-diseases.onrender.com/";
