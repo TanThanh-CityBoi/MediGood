2e4de62d37207adfa49ac76e649c6c8e850373ea
hacthe-codelink <the.duong@codelink.io>
2023-01-29 08:15:55 +0700

    config app

--- src/views/layout-components/TheContent.js
+++ src/views/layout-components/TheContent.js
@@ -1,18 +1,27 @@
-import React from 'react'
-import { Outlet } from 'react-router-dom'
-import TheHeader from './TheHeader'
-import TheFooter from './TheFooter'
-import ConfirmActionModal from '../component/ConfirmActionModal'
+import React from "react";
+import { Outlet } from "react-router-dom";
+import TheHeader from "./TheHeader";
+import TheFooter from "./TheFooter";
+import ConfirmActionModal from "../component/ConfirmActionModal";
+import BotChat from "../../views/component/chat-component/BotChat/BotChat";
+import ChatBox from "../../views/component/chat-component/Chat/Chat";
+import { useSelector } from "react-redux";
 
 const TheContent = () => {
+  const authentication = useSelector((state) => state.userReducer);
+
   return (
     <>
-    <ConfirmActionModal/>
+      <ConfirmActionModal />
       <TheHeader></TheHeader>
       <Outlet></Outlet>
       <TheFooter></TheFooter>
+      <div className="floating-boxchats">
+        <BotChat />
+        {authentication.isLoggedIn && <ChatBox />}
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default React.memo(TheContent)
+export default React.memo(TheContent);
